a00485e47c7116c8b3cdf1bf6798c0ea0b949389
Taka <ttung@futuredial.com>
2023-11-24 12:56:30 +0800

    chore(tailwind.config): config theme color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,20 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      colors: {
+        'amber': '#f9b572',
+        'green': '#9ad0c2',
+        'red': '#ffc5c5',
+        'yellow': '#feff86',
+        'off-white': '#fffbf5',
+        'purple': '#d0a2f7'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
