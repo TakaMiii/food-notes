a86f9324d59e062c5d1f0483ce0aa525a98031e7
Taka <ttung@futuredial.com>
2023-11-24 12:54:57 +0800

    refactor

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import FoodGroups from 'src/components/FoodGroups.js'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <FoodGroups />
+    </main>
+  )
+}
