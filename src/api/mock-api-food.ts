52a06539236fceafb0c0244b5ae570d4f446818a
Taka <ttung@futuredial.com>
2023-11-29 16:08:38 +0800

    refactor(api-foods.ts): Refactored data format

--- src/api/mock-api-food.ts
+++ src/api/mock-api-food.ts
@@ -1,89 +1,106 @@
-type foodInfo = { value: {id: string, unit: string}, label: string };
+type foodInfo = {
+    value: { id: string, unit: string },
+    label: string,
+    group: string
+};
 
-const grains:foodInfo[] = [
-  {
-    value: {id: 'rice', unit: '碗'},
-    label: '米',
-  },
-  {
-    value: {id: 'noodle', unit: '碗'},
-    label: '麵',
-  },
-  {
-    value: {id: 'bread', unit: '片'},
-    label: '麵包',
-  },
-  {
-    value: {id: 'wheat',unit: '碗'},
-    label: '小麥',
+const data: foodInfo[] = [
+    {
+        value: {id: 'rice', unit: '碗'},
+        label: '米',
+        group: 'grain'
+    },
+    {
+        value: {id: 'noodle', unit: '碗'},
+        label: '麵',
+        group: 'grain'
+    },
+    {
+        value: {id: 'bread', unit: '片'},
+        label: '麵包',
+        group: 'grain'
+    },
+    {
+        value: {id: 'wheat', unit: '碗'},
+        label: '小麥',
 
-  },
-  {
-    value: {id: 'corn', unit: '個'},
-    label: '玉米',
-  },
-  {
-    value: {id: 'potato', unit: '個'},
-    label: '馬鈴薯',
-  },
-  {
-    value: {id: 'sweet-potato', unit: '個'},
-    label: '蕃薯',
-  },
-  {
-    value: {id: 'pumpkin', unit: '個'},
-    label: '南瓜',
-  },
+        group: 'grain'
+    },
+    {
+        value: {id: 'corn', unit: '個'},
+        label: '玉米',
+        group: 'grain'
+    },
+    {
+        value: {id: 'potato', unit: '個'},
+        label: '馬鈴薯',
+        group: 'grain'
+    },
+    {
+        value: {id: 'sweet-potato', unit: '個'},
+        label: '蕃薯',
+        group: 'grain'
+    },
+    {
+        value: {id: 'pumpkin', unit: '個'},
+        label: '南瓜',
+        group: 'grain'
+    },
+    {
+        value: {id: 'egg', unit: '個'},
+        label: '蛋',
+        group: 'protein'
+    },
+    {
+        value: {id: 'beef', unit: 'g'},
+        label: '牛',
+        group: 'protein'
+    },
+    {
+        value: {id: 'pork', unit: 'g'},
+        label: '豬',
+        group: 'protein'
+    },
+    {
+        value: {id: 'chicken', unit: 'g'},
+        label: '雞',
+        group: 'protein'
+    },
+    {
+        value: {id: 'duck', unit: 'g'},
+        label: '鴨',
+        group: 'protein'
+    }
 ];
 
-const proteins:foodInfo[] = [
-  {
-    value: {id: 'egg', unit: '個'},
-    label: '蛋',
-  },
-  {
-    value: {id: 'beef', unit: 'g'},
-    label: '牛',
-  },
-  {
-    value: {id: 'pork', unit: 'g'},
-    label: '豬',
-  },
-  {
-    value: {id:'chicken', unit: 'g'},
-    label: '雞',
-  },
-  {
-    value: {id: 'duck', unit: 'g'},
-    label: '鴨',
-  }
-]
 
 function mockGrainsApi() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(grains);
-    }, 2000);
-  });
+    return new Promise((resolve) => {
+        const grains = data.filter(food => food.group === 'grain');
+        setTimeout(() => {
+            resolve(grains);
+        }, 2000);
+    });
 }
 
 function mockProteinsApi() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(proteins);
-    }, 2000);
-  });
+    return new Promise((resolve) => {
+        let proteins = data.filter(food => food.group === 'proteins');
+        setTimeout(() => {
+            resolve(proteins);
+        }, 2000);
+    });
 }
-async function getFoodOptions(groupName:string) {
-  if(groupName === 'grain') {
-    const options = await mockGrainsApi() as {value: string, label: string}[];
-    return options
-  }
-  if(groupName === 'protein') {
-    const options = await mockProteinsApi() as {value: string, label: string}[];
-    return options
-  }
 
+async function getFoodOptions(groupName: string) {
+    if (groupName === 'grain') {
+        const options = await mockGrainsApi() as { value: string, label: string }[];
+        return options;
+    }
+    if (groupName === 'protein') {
+        const options = await mockProteinsApi() as { value: string, label: string }[];
+        return options;
+    }
 }
 
 export {getFoodOptions};
